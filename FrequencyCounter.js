1259b40401eb4b24ece880fa50f3ccf4602df28f
Amine <elouartinra@hotmail.com>
2019-07-04 00:29:18 +0100

    Added the porblem cuz i forgot to past it

--- FrequencyCounter.js
+++ FrequencyCounter.js
@@ -1,28 +1,37 @@
 //O(n)
 // useful for comparing bunch of pieces of data.
 
+//The problem
+/** Write a function which accepts two arrays. The function should return
+true if every value in the first array has a corresponding value in the second 
+array. The frequency of values must be the same.*/
+
 //for testing
-const arrT1 = [1,1,4,5,3]
-const arrT2 = [4,1,1,5,3]
+const arrT1 = [1, 1, 4, 5, 3];
+const arrT2 = [4, 1, 1, 5, 3];
 
-const compareArr = (arr1,arr2)=>{
-  if(arrT1.length !== arr2.length ) {return false;}
-  const freqCounter1={}
-  const freqCounter2={}
-  arr1.map((item)=>{
-    freqCounter1[item] = (freqCounter1[item] || 0 ) +1
-  })
-  arr2.map((item)=>{
-    freqCounter2[item] = (freqCounter2[item] || 0) + 1
-  })
-  for(let key in freqCounter1){
-    if(!(key in freqCounter1)){return false}
-    if(freqCounter1[key]!==freqCounter2[key]){
-      return false
-    }
-  }
+const compareArr = (arr1, arr2) => {
+	if (arrT1.length !== arr2.length) {
+		return false;
+	}
+	const freqCounter1 = {};
+	const freqCounter2 = {};
+	arr1.map(item => {
+		freqCounter1[item] = (freqCounter1[item] || 0) + 1;
+	});
+	arr2.map(item => {
+		freqCounter2[item] = (freqCounter2[item] || 0) + 1;
+	});
+	for (let key in freqCounter1) {
+		if (!(key in freqCounter1)) {
+			return false;
+		}
+		if (freqCounter1[key] !== freqCounter2[key]) {
+			return false;
+		}
+	}
 
-  return true
-}
+	return true;
+};
 
-console.log(compareArr(arrT1,arrT2)) // true
+console.log(compareArr(arrT1, arrT2)); // true
